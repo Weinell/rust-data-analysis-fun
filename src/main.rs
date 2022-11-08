ff9f8aaeb5f5f65cad197525cd8ffe6f65b8697f
Nikolaj Weinell <nikolajweinell@hotmail.com>
2022-11-09 00:20:32 +0100

    Read person

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
-use std::thread;
-use std::fs::{File, write};
+use serde::{Deserialize, Serialize};
+use serde_json::{self};
+use std::fs::File;
+use std::io::BufReader;
+use std::error::Error;
+use std::path::Path;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Person {
+    name: String,
+    age: u8,
+    gender: String,
+}
 
 fn main() {
-    
     let data_path: &str = "./data/data.json";
-    let text = std::fs::read_to_string(data_path).unwrap();
-    let json: serde_json::Value = serde_json::from_str(&text).expect("Json file error");
-    println!("data :{}",json)
+
+    println!("{:?}",read_person_from_file(data_path));
+
+}
+
+fn read_person_from_file<P: AsRef<Path>>(path: P) -> Result<Person, Box<dyn Error>> {
+
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    let u = serde_json::from_reader(reader)?;
+
+    Ok(u)
 }
