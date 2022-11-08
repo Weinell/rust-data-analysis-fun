645c8546546eaf04a121d1a7191f16c02b6fb953
Nikolaj Weinell <nikolajweinell@hotmail.com>
2022-11-08 20:58:21 +0100

    Open and print data

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::thread;
+use std::fs::{File, write};
 
 fn main() {
     
-    
-
+    let data_path: &str = "./data/data.json";
+    let text = std::fs::read_to_string(data_path).unwrap();
+    let json: serde_json::Value = serde_json::from_str(&text).expect("Json file error");
+    println!("data :{}",json)
 }
